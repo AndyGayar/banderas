1071b93a5d0e03669347cec937d5ac89d6e2e8fb
AndyGayar <andresgayarromero@gmail.com>
2025-02-10 19:32:07 +0100

    banderitas

--- js/banderas.js
+++ js/banderas.js
@@ -1,15 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-xhr.onload = function () {
-  if (xhr.status === 200) {
-    var data = JSON.parse(xhr.responseText);
-    var banderas = document.getElementById("banderas");
-    for (var i in data) {
-      var img = document.createElement("img");
-      img.src = data[i].flags.png;
-      img.alt=data[i].name.common;
-      banderas.appendChild(img);
-    }
+
+fetch("https://restcountries.com/v3.1/all")
+.then(response => response.json())
+.then(data => {
+  var banderas = document.getElementById("banderas");
+  for (var i in data) {
+    var img = document.createElement("img");
+    img.src = data[i].flags.png;
+    img.alt=data[i].name.common;
+    banderas.appendChild(img);
   }
-};
-xhr.send();
+})
+.catch(error => console.error(error));
