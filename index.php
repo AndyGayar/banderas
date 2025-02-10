425d56b93d3f24d7a63faf7c033cbdc60c07a991
AndyGayar <andresgayarromero@gmail.com>
2025-02-10 19:00:38 +0100

    banderita

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/banderas.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Banderas</h1>
+    </header>
+    <main id="banderas"></main>
+    <footer>
+        <p>Desarrollado por a p</p>
+    </footer>
+</body>
+</html>
